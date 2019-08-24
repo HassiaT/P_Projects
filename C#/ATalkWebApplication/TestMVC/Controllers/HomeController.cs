968dc4ad4f314502c0472c5a468f7af0f5d6c964
Hassia T <hassia.tamimou@gmail.com>
2019-08-24 23:29:49 +0200

    Web Application project

---
+++ C#/ATalkWebApplication/TestMVC/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return RedirectToAction("Register", "AuthenticationController");
+        }
+
+        public ActionResult Login()
+        {
+            // if User already has an account --->> found in the database
+            //RedirectToRoute(Go to room 0);
+            // return RedirectToAction("Index", "ChatController");
+            //If NOT print message that password and id are WRONG but stays on the same page
+       
+            return RedirectToAction("Login", "AuthenticationController");
+        }
+
+
+        public ActionResult Forgot()
+        {
+            return View();
+        }
+    }
+}

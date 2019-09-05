c954d00b852eafad3367e2bc82e9b96ff17637ea
Hassia T <hassia.tamimou@gmail.com>
2019-09-05 18:23:00 +0200

    C++ Kingdom connectivity adding test cases

--- Miscellaneous/C++/HardChallenges/HardChallenges/HardChallenges.cpp
+++ Miscellaneous/C++/HardChallenges/HardChallenges/HardChallenges.cpp
@@ -54,11 +54,9 @@ void dfs(int source, map<int, vector<int>>*neighbors, int destination,
 	marked[source] = 1; //VISITING
 
 	vector<int> current_neighors = (*neighbors)[source];
-
 	for (int i = 0; i < current_neighors.size(); i++)
 	{
 		int current_node = current_neighors[i];
-
 		if (marked[current_node] == 0)
 		{
 			vector<int> next_neighbors = (*neighbors)[current_node];
@@ -82,47 +80,9 @@ void countPaths(int n, vector<vector<int>> edges, int &countPaths) {
 	int source = 1;
 	int destination = 5;
 	int*m_marked = new int[n + 1];
-	
-	//Initialization:
 	for (int u = 0; u < n; u++)
 		m_marked[u] = 0;
-
-	//if node 1 is not connected to any node:
-	//if ( (*allNeighbors)[source].size() == 0 )
+	
 	dfs(source, allNeighbors, destination, countPaths, m_marked);
 	cout << "COUNT PATHS : " << countPaths << endl;
 }
-
-
-//int main()
-//{
-//	int n = 5;
-//	//vector < vector<int>> edges = { {1, 2},{2,4},{2,3},{3,4},{4,5} }; // ANSWER:  2
-////	vector<vector<int>> edges = { {1,2},{4,2},{2,3},{3,4},{4,5} }; // ANSWER:  INFINITE PATHS
-//	vector<vector<int>> edges = { {4,2},{2,3},{3,4},{4,5} }; // ANSWER:  0 PATHS
-//
-//	map<int, vector<int>>*allNeighbors = findNeighbors(n, edges);
-//	print_out(allNeighbors);
-//	int c = 0;
-//	countPaths(n, edges, c);
-//	delete allNeighbors;
-//	cout << "Press any keyboard \n";
-//	int r;
-//	cin >> r;
-//}
-
-
-
-
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
--- Miscellaneous/C++/HardChallenges/HardChallengesTest/test.cpp
+++ Miscellaneous/C++/HardChallenges/HardChallengesTest/test.cpp
@@ -7,11 +7,14 @@
 
 TEST(threeCases, countPaths) {
 	int n = 5;
-	// vector < vector<int>> edges = { {1, 2},{2,4},{2,3},{3,4},{4,5} }; // ANSWER:  2
-	// vector<vector<int>> edges = { {1,2},{4,2},{2,3},{3,4},{4,5} }; // ANSWER:  INFINITE PATHS
-	vector<vector<int>> edges = { {4,2},{2,3},{3,4},{4,5} }; // ANSWER:  0 PATHS
+	vector < vector<int>> edges = { {1, 2},{2,4},{2,3},{3,4},{4,5} }; // ANSWER:  2
+   //vector<vector<int>> edges = { {1,2},{4,2},{2,3},{3,4},{4,5} }; // ANSWER:  INFINITE PATHS
+   //vector<vector<int>> edges = { {4,2},{2,3},{3,4},{4,5} }; // ANSWER:  0 PATHS
 
-	GTEST_COUT << "0" << std::endl;
+   //GTEST_COUT << "0" << std::endl;
+   //GTEST_COUT << "INFINITE PATHS" << std::endl;
+	GTEST_COUT << "2" << std::endl;
+
 }
 
 int main(int argc, char** argv)

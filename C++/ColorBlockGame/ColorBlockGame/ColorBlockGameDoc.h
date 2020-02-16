c24bfae6d07bb1bd84e2a5928f6250dc477c0ef9
Hassia T <hassia.tamimou@gmail.com>
2020-02-16 22:44:11 +0100

    Front end of Color Block Game project

---
+++ C++/ColorBlockGame/ColorBlockGame/ColorBlockGameDoc.h
@@ -0,0 +1,72 @@
+
+// ColorBlockGameDoc.h : interface of the CColorBlockGameDoc class
+//
+#pragma once
+#include "ColorBlockBoard.h"
+
+class CColorBlockGameDoc : public CDocument
+{
+protected: // create from serialization only
+	CColorBlockGameDoc() noexcept;
+	virtual ~CColorBlockGameDoc();
+	DECLARE_DYNCREATE(CColorBlockGameDoc)
+
+	// Attributes
+public:
+
+	// Operations
+public:
+
+	COLORREF getcolorcell(int _row, int _column) {
+		return theboard.getcolorcell(_row, _column);
+	}
+	void setupboard(void) {
+		theboard.setupboard();
+	}
+	int Getboardwidth() {
+		return theboard.Getboardwidth();
+	}
+	int Getboardheight() {
+		return theboard.Getboardheight();
+	}
+	void deleteboard(void)
+	{
+		theboard.deleteboard();
+	}
+	int Getboardcolumns() {
+		return theboard.Getboardcolumns();
+	}
+	int Getboardrows() {
+		return theboard.Getboardrows();
+	}
+
+
+	// Overrides
+public:
+	virtual BOOL OnNewDocument();
+	virtual void Serialize(CArchive& ar);
+#ifdef SHARED_HANDLERS
+	virtual void InitializeSearchContent();
+	virtual void OnDrawThumbnail(CDC& dc, LPRECT lprcBounds);
+#endif // SHARED_HANDLERS
+
+	// Implementation
+public:
+	virtual ~CColorBlockGameDoc();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+	ColorBlockBoard<int> theboard;
+
+	// Generated message map functions
+protected:
+	DECLARE_MESSAGE_MAP()
+
+#ifdef SHARED_HANDLERS
+	// Helper function that sets search content for a Search Handler
+	void SetSearchContent(const CString& value);
+#endif // SHARED_HANDLERS
+};
